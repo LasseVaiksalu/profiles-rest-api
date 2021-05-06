24a5ad55ad03fdffd37b2d3c7056e38e92915a61
Lasse Vaiksalu <lasse.vaiksalu@gmail.com>
2021-05-06 16:41:59 +0300

    Added viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,4 +3,5 @@ from rest_framework import serializers
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing out APIView"""
+    """ Serializer converts data inputs to python objects and vice-versa"""
     name = serializers.CharField(max_length=10)
